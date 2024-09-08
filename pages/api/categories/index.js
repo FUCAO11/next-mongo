40fe621f01b920e8553b2e449f675c8bd80340f2
曹福 <caofu@caofudeMacBook-Air.local>
2024-09-08 13:31:50 +0700

    更新了文件路径以修复部署错误

--- pages/api/categories/index.js
+++ pages/api/categories/index.js
@@ -1,5 +1,5 @@
 import dbConnect from '../../../lib/db';
-import Category from '../../../models/category';
+import Category from '../../../models/Category';
 
 export default async function handler(req, res) {
   await dbConnect();
